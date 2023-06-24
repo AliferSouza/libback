fbdddeae87dec61e5367a95af44148970af5b267
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-06-24 00:22:42 -0300

    Update mais.js

--- api/mais.js
+++ api/mais.js
@@ -1,12 +1,13 @@
+// mais.js
 const path = require('path');
 const fs = require('fs');
 module.exports = async function(req, res) {
-   const parentDirectoryPath = path.resolve(__dirname, '..',  'src', 'index.html');
+   const parentDirectoryPath = path.resolve(__dirname, '..',  'db', 'db.json');
   try {
     const content = await fs.promises.readFile(parentDirectoryPath, 'utf-8');
     console.log(content)
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'application/json');
     res.end(content, 'utf-8');
   } catch (err) {
     res.statusCode = 500;
