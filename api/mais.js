dcaa0c71fb62a352b2dbdacc4768e27458b55218
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-06-24 00:19:43 -0300

    Update mais.js

--- api/mais.js
+++ api/mais.js
@@ -1,8 +1,18 @@
-
 const path = require('path');
 const fs = require('fs');
-module.exports =  async function (req, res) {
-  const filePath = path.resolve(__dirname, "..", "src", 'index.html');
+module.exports = async function(req, res) {
+   const parentDirectoryPath = path.resolve(__dirname, '..',  'src', 'index.html');
+  try {
+    const content = await fs.promises.readFile(parentDirectoryPath, 'utf-8');
+    console.log(content)
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(content, 'utf-8');
+  } catch (err) {
+    res.statusCode = 500;
+    res.end('Erro ao ler o arquivo JSON.');
+    }
+  
 
-
-};
+  };
+  
