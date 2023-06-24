ec336a18ec643e9d0804784a28fe8000a5ace428
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-06-23 22:40:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,68 @@
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const { server, router} = require('./server.js');
-
-function resolverApiDiretorio() {
-  const functionDirectory = './api'; // Diretório com as funções (ajuste o caminho conforme necessário)
-  const apiDirectory = './api'; // Diretório onde será salvo o arquivo "apis.js" (ajuste o caminho conforme necessário)
-
-  function generateAPIsObject() {
-    const apis = {};
-
-    // Lê os arquivos presentes no diretório das funções
-    const files = fs.readdirSync(functionDirectory);
-    const jsFiles = files.filter(file => file.endsWith('.js'));
-    jsFiles.forEach(file => {
-      const functionName = path.parse(file).name;
-      // Ignora o arquivo se o nome for 'index'
-      if (functionName !== 'index') {
-        apis[functionName] = file;
-      }
-    });
-
-    return apis;
-  }
-
-  const apisObject = generateAPIsObject();
-
-  let declarations = '';
-  Object.keys(apisObject).forEach(functionName => {
-    const filePath = path.join(apisObject[functionName]);
-    const declaration = `const ${functionName} = require('./api/${functionName}');\n`;
-    declarations += declaration;
-  });
-
-  const fileContent = `
-function fileContentApi() {
-${declarations}
-  return module.exports = {
-${Object.entries(apisObject)
-    .map(([key, value], index, array) => {
-      if (index === array.length - 1) {
-        return `    ${key}`;
-      } else {
-        return `    ${key},`;
-      }
-    })
-    .join('\n')}
-  };
-}
-
-fileContentApi();`;
-
-  return fileContent;
-}
-const pathApi = resolverApiDiretorio();
-const apiObject = eval(pathApi);
-
-
-router.get('/', (req, res) => {
-  const filePath = path.join(__dirname,  'index.html');
-
-  fs.readFile(filePath, (err, content) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(content, 'utf-8');
-   
-  });
-});
-
-
-
-
-router.all('/api/:id', async (req, res) => {
-  const apiDirectory = path.join(__dirname, 'api');
-  const id = req.params.id;
-  const apiEndpoint = req.url.replace('/router', '');
-  const filePath = path.join(apiDirectory, apiEndpoint + '.js');
-  apiObject[id](req, res)
-
-  
-});
-
-
 
 const port = process.env.PORT || 3000;
+
+// Função para resolver o diretório das APIs
+function resolverApiDiretorio() {
+  const functionDirectory = './api'; // Diretório com as funções (ajuste o caminho conforme necessário)
+
+  const apiObject = {};
+
+  // Lê os arquivos presentes no diretório das funções
+  const files = fs.readdirSync(functionDirectory);
+  const jsFiles = files.filter(file => file.endsWith('.js'));
+
+  jsFiles.forEach(file => {
+    const functionName = path.parse(file).name;
+    // Ignora o arquivo se o nome for 'index'
+    if (functionName !== 'index') {
+      const apiFunction = require(path.join(functionDirectory, file));
+      apiObject[functionName] = apiFunction;
+    }
+  });
+
+  return apiObject;
+}
+
+const apiObject = resolverApiDiretorio();
+
+// Criação do servidor
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  // Rota para as APIs
+  if (url.startsWith('/api/')) {
+    const id = url.substring(5);
+    if (apiObject.hasOwnProperty(id)) {
+      const apiFunction = apiObject[id];
+      apiFunction(req, res);
+    } else {
+      res.statusCode = 404;
+      res.end('API endpoint not found');
+    }
+  } else if (url === '/') {
+    // Rota padrão (index.html)
+    const filePath = path.join(__dirname, 'public', 'index.html');
+    fs.readFile(filePath, 'utf-8', (err, content) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(content);
+      }
+    });
+  } else {
+    // Rota inválida
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
+
+// Inicia o servidor
 server.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
-
-
